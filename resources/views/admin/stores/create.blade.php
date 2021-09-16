15803994d6f72e630bceb06a4f7f62229402609c
Rafael Arend <rafinhaarend123@hotmail.com>
2021-09-16 16:34:16 -0400

    finalizado seção 6

---
+++ resources/views/admin/stores/create.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Criar loja</h1>
+
+    <form action="{{route('admin.stores.store')}}" method="post">
+
+        <input type="hidden" name="_token" value="{{csrf_token()}}">
+
+        <div class="form-group">
+            <label>Loja</label>
+            <input type="text" name="name" class='form-control'>
+        </div>
+
+        <div class="form-group">
+            <label>Descrição</label>
+            <input type="text" name="description" class='form-control'>
+        </div>
+
+        <div class="form-group">
+            <label>Telefone</label>
+            <input type="text" name="phone" class='form-control'>
+        </div>
+
+        <div class="form-group">
+            <label>Celular</label>
+            <input type="text" name="mobile_phone" class='form-control'>
+        </div>
+
+        <div class="form-group">
+            <label>Slug</label>
+            <input type="text" name="slug" class='form-control'>
+        </div>
+
+        <div class="form-group">
+            <label>Usuário</label>
+            <select name="user">
+                @foreach($users as $user)
+                    <option value="{{$user->id}}">{{$user->name}}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div>
+            <button type="submit" class="btn btn-lg btn-success">Confirmar</button>
+        </div>
+    </form>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marketplace L6</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <a href="{{route('admin.stores.create')}}" class="btn btn-lg btn-success">Criar Loja</a>
+        <a href="{{route('admin.stores.index')}}" class="btn btn-lg btn-success">Listar Lojas</a>
+
+        @include('flash::message')
+        @yield('content')
+    </div>
+</body>
+</html>
